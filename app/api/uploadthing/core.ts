784a2a27070e0f84b4e710bb78428c4722618a7b
itzVort3x1 <saikaustubh10@gmail.com>
2023-09-12 07:33:02 +0530

    updated core.ts

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -9,7 +9,6 @@ const handleAuth = () => {
 	return { userId: userId };
 };
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
 	// Define as many FileRoutes as you like, each with a unique routeSlug
 	serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
